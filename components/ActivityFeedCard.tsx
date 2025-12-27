5466bff66d8ddeda44bb09ba93163167b5b96c93
Mithilesh Kumar <mithileshk.in@gmail.com>
2025-12-27 16:24:13 +0000

    x video embedding

--- components/ActivityFeedCard.tsx
+++ components/ActivityFeedCard.tsx
@@ -811,7 +811,7 @@ export default function ActivityFeedCard({ activity }: ActivityFeedCardProps) {
 
         {/* Video embed */}
         {activity.video_url && (
-          <div className="w-full bg-black/5 px-4 md:px-4 mb-4">
+          <div className="w-full bg-black/5 px-4 md:px-4 mb-8 clear-both">
             <div className="w-full max-w-3xl mx-auto">
               {activity.video_url.includes('drive.google.com') ? (
                 <div className="drive-video-embed w-full aspect-video bg-gray-100 rounded-md overflow-hidden">
@@ -825,16 +825,19 @@ export default function ActivityFeedCard({ activity }: ActivityFeedCardProps) {
                   ></iframe>
                 </div>
               ) : activity.video_url.includes('twitter.com') || activity.video_url.includes('x.com') ? (
-                <div className="twitter-video-embed w-full bg-gray-100 rounded-md overflow-hidden flex justify-center">
-                  <iframe 
-                    src={`https://platform.x.com/embed/Tweet.html?id=${activity.video_url.match(/\/status\/([0-9]+)/)?.[1]}`}
-                    width="100%" 
-                    height="400" 
-                    frameBorder="0" 
-                    scrolling="no" 
-                    allowFullScreen
-                    className="w-full min-h-[400px] max-w-2xl"
-                  ></iframe>
+                <div className="twitter-video-embed w-full bg-gray-100 rounded-md overflow-hidden flex justify-center py-4">
+                  <div className="w-full flex justify-center">
+                    <iframe 
+                      src={`https://platform.x.com/embed/Tweet.html?id=${activity.video_url.match(/\/status\/([0-9]+)/)?.[1]}`}
+                      width="100%" 
+                      height="600" 
+                      frameBorder="0" 
+                      scrolling="no" 
+                      allowFullScreen
+                      className="w-full max-w-md md:max-w-lg lg:max-w-2xl border-0"
+                      style={{ minHeight: '600px' }}
+                    ></iframe>
+                  </div>
                 </div>
               ) : activity.video_url.includes('youtube.com') || activity.video_url.includes('youtu.be') ? (
                 <div className="youtube-embed">
